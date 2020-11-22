4f6acdc3ad827ea32cc8b383a7ae510a16600b5f
mimbaby <mimbaby@egypt.itgsolutions.com>
2020-11-22 12:37:48 +0200

    add grid view

---
+++ lib/Strings.dart
@@ -0,0 +1,7 @@
+class Strings {
+  static String CUSTOM_CHART  =   "assets/custom.png";
+  static String WEEKLY_CHART  =   "assets/week.png";
+  static String MONTHLY_CHART =   "assets/month.png";
+  static String YEARLY_CHART  =   "assets/yearly.png";
+  static String MULTILINE_CHART = "assets/multi.png";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'Strings.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -80,30 +82,16 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+        child: GridView(
+    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+    crossAxisCount: 2,
+    childAspectRatio: 2.0),
+          children: [
+            cardView(title: "Custom Chart" ,image :Strings.CUSTOM_CHART ),
+            cardView(title: "Weekly Chart" , image :Strings.WEEKLY_CHART),
+            cardView(title: "Monthly Chart" ,image :Strings.MONTHLY_CHART ),
+            cardView(title: "Year Chart" ,image :Strings.YEARLY_CHART ),
+            cardView(title: "Custom Numbers multiline" ,image :Strings.MULTILINE_CHART ),
           ],
         ),
       ),
@@ -113,5 +101,26 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget cardView  ( {String title , String image }) {
+    return Container(
+      width: 200,
+      height: 120,
+      alignment: Alignment.center,
+      child: ListView(
+        children: [
+                  Image.asset(image, height: 60,),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(title, style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
+                    ],
+                  )
+              ],
+      ),
+    );
+
   }
 }
